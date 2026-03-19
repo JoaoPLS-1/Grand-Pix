4909429dc493af962a8cc521c27eae5af6849ccd
JoaoPLS-1 <barrancoij@gmail.com>
2026-03-19 19:10:45 -0300

    fat: forcando valores do supabaseUrl e supabaseKey

--- pages/lib/supabase.ts
+++ pages/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://wfjxvvrpuwsbzuqsfrni.supabase.co'
+const supabaseKey = 'sb_publishable_8WOfJw9OrTyDncBbHkaAaQ_d7sUYl7C'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
